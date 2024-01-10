b8022cae750b9c461547287f2d03d7304e385cde
Kang Sanghyun <kangsang3073@gmail.com>
2024-01-10 19:13:19 +0900

    로그인페이지 추가

--- src/components/InputBox/index.tsx
+++ src/components/InputBox/index.tsx
@@ -11,7 +11,7 @@ interface Props {
     setValue: Dispatch<SetStateAction<string>>;
     error: boolean;
 
-    icon?: string;
+    icon?: 'eye-light-off-icon' | 'eye-light-on-icon' | 'expend-right-light-icon';
     onButtonClick?: () => void;
 
     message?: string;
